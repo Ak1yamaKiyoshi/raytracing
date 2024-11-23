fa8a242f6d7776f99b73d8e967e4faca2d6ca140
budulka <azazasapli@gmail.com>
2024-11-23 16:30:49 +0200

    finished basic operations

--- budulka/main.c
+++ budulka/main.c
@@ -50,7 +50,7 @@ struct vec3d add(struct vec3d a, struct vec3d b) {
 }
 
 
-struct vec3d substract(struct vec3d a, struct vec3d b) {
+struct vec3d subtract(struct vec3d a, struct vec3d b) {
   struct vec3d r;
   r.x = a.x - b.x;
   r.y = a.x - b.x;
@@ -58,6 +58,29 @@ struct vec3d substract(struct vec3d a, struct vec3d b) {
   return r;
 }
 
+struct vec3d crossproduct(struct vec3d a, struct vec3d b) {
+	struct vec3d r;
+	r.x = a.y * b.z - b.y * a.z;
+	r.y = a.z * b.x - b.z * a.x;
+	r.z = a.x * b.y - a.y * b.x;
+	return r;
+}
+
+
+struct vec2d multiply2d_by_scalar(struct vec2d a, double k) {
+	struct vec2d r;
+	r.x = a.x * k;
+	r.y = a.y * k;
+	return r;
+} 
+
+struct vec3d multiply3d_by_scalar(struct vec3d a, double k) {
+	struct vec3d r;
+	r.x = a.x * k;
+	r.y = a.y * k;
+	r.z = a.z * k;
+	return r;
+}
 
 void print(struct vec3d v) {
   printf("vec3d(%.4lf, %.4lf, %.4lf)", v.x, v.y, v.z);
@@ -65,15 +88,18 @@ void print(struct vec3d v) {
 
 
 int main() {
-  vec3d a = {1.f, 2.f, 3.f};
-  vec3d b = {2.f, 3.f, 4.f};
+  struct vec3d a = {1.f, 2.f, 3.f};
+  struct vec3d b = {2.f, 3.f, 4.f};
   
   print(add(a, b));
   printf("\n");
-  print(substract(a, b));
-  printf("dot product: %lf", dotproduct(a, b));
+  print(subtract(a, b));
+  printf("dot product: %lf\n", dotproduct(a, b));
   print(multiply(a, b));
   print(divide(a, b));  
-  printf("test");
+  printf("test\n");
+  struct vec3d r = crossproduct(a, b);
+  printf("cross_product: vector{%lf, %lf, %lf}", r.x, r.y, r.z);
+
   return 0;
 }
